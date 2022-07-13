6c910b8ce76f00df37dff28997b9243cca5b02b1
Kevin Arce <kevin_19.6@hotmail.com>
2022-07-12 22:14:14 -0600

    Segundo Ejercicio

---
+++ Semana 2 clase 1/Clases.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana_2_clase_1
+{
+    //clase abstrapta
+    abstract class Animal
+    {
+        public abstract void Reproducirse();
+
+        public void moverser()
+        {
+            Console.WriteLine("El animal se esta moviendo. ");
+        }
+    }
+
+    class Reptil : Animal 
+    {
+        // Metodo abstrapto 
+        public override void Reproducirse()
+        {
+            Console.WriteLine("El reptil anida huevos.");
+        }
+    }
+
+
+
+
+
+
+
+
+    // clase padre
+    class Mamifero : Animal
+       
+    {
+        //metodo abstrapto
+        public override void Reproducirse()
+        {
+            Console.WriteLine("El Humano da a luz en un parto");
+        }
+
+        //Atributo
+       public string Nombre { get; set; }
+
+        //metodos
+        public void Darnombre()
+        {
+            Console.WriteLine("Mi nombre es " + Nombre);
+        }
+    }
+
+    //Clases hijas
+    class Humano : Mamifero
+    {
+        public void Hablar()
+            {
+            Console.WriteLine("El humano esta hablando. ");
+            }
+    }
+
+    class Caballo : Mamifero
+    {
+        public void Galopar()
+        {
+            Console.WriteLine("El caballo galopa. ");
+        }
+    }
+
+    class Delfin : Mamifero
+    {
+        public void Nadar()
+        {
+            Console.WriteLine("El delfin nada. ");
+        }
+    }
+
+    class Gorila : Mamifero
+    {
+        public void Trepar()
+        {
+            Console.WriteLine("El gorila sube el arbol. ");
+        }
+    }
+}
---
+++ Semana 2 clase 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Semana_2_clase_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Humano humano1 = new Humano();
+            humano1.Nombre = "Kevin";
+            humano1.Darnombre();
+            humano1.Hablar();
+            humano1.Reproducirse();
+
+            Caballo caballo1 = new Caballo();
+            caballo1.Nombre = "Silver";
+            caballo1.Galopar();
+
+            Delfin delfin1 = new Delfin();
+            delfin1.Nombre = "winter";
+            delfin1.Nadar();
+
+            Gorila gorila1 = new Gorila();
+            gorila1.Nombre = "King";
+            gorila1.Trepar();
+
+            Reptil iguana = new Reptil();
+
+            iguana.Reproducirse();
+
+        }
+    }
+}
